b1f8fffa18ca75c3b019f6e6b9793dc598690def
serkanakgok <serkan.akgok@hotmail.com>
2022-01-14 22:30:58 +0300

    rütuş yapıldı

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,11 @@ public class Main {
         if (alinan > 0 && alinan < 1000) {
             double kdv1 = (kdvsiz * 18) / 100 + alinan;
             double tutar1 = (kdvsiz * 18) / 100;
-            System.out.println("KDV'siz Fiyat: " + alinan + "\nKDV'li Fiyat: " + kdv1 + "\nKDV Tutarı: " + tutar1);
+            System.out.println("KDV'siz Fiyat: " + alinan + "\nKDV oranı: 18%" + "\nKDV'li Fiyat: " + kdv1 + "\nKDV Tutarı: " + tutar1);
         } else if (alinan >= 1000) {
             double kdv2 = (kdvsiz * 8) / 100 + alinan;
             double tutar2 = (kdvsiz * 8) / 100;
-            System.out.println("KDV'siz Fiyat: " + alinan + "\nKDV'li Fiyat: " + kdv2 + "\nKDV Tutarı: " + tutar2);
+            System.out.println("KDV'siz Fiyat: " + alinan + "\nKDV oranı: 8%" + "\nKDV'li Fiyat: " + kdv2 + "\nKDV Tutarı: " + tutar2);
 
         } else if (alinan==0){
             System.out.println("Sıfır diye para mı olur fakir.");
